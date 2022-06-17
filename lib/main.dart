ca07e49ed50562c2f808b6dcf5e05f728e84f907
FreshDev <billyhans90@gmail.com>
2022-06-17 15:38:42 +0100

    Use homepage in main function

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,20 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
 }
 
+// Home Page
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
