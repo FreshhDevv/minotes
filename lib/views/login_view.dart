a51619aa1feb076b55c4594493bbc6d028560895
FreshDev <billyhans90@gmail.com>
2022-06-25 21:01:47 +0100

    Error handling in the login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import 'package:minotes/constants/routes.dart';
+
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -21,7 +22,7 @@ class _LoginViewState extends State<LoginView> {
     _password = TextEditingController();
     super.initState();
   }
- 
+
   @override
   void dispose() {
     _email.dispose();
@@ -66,10 +67,26 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found');
+                  await showErrorDialog(
+                    context,
+                    'User not found',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Wrong password');
+                  await showErrorDialog(
+                    context,
+                    'Wrong credentials',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
@@ -87,3 +104,5 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
+
+
